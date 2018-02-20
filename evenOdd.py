8341ea1694c515cffe6a514acd0512a382812357
NurseQ <jamesleroyq@gmail.com>
2018-02-20 00:57:50 +0000

    Script to check if a given number is odd or even

---
+++ evenOdd.py
@@ -0,0 +1,10 @@
+#James Quintin 09/02/18
+#code to check if an int is odd or even
+
+x = int(input("Please enter your no: "))
+
+if x % 2 == 0:
+    
+    print("It is even!")
+else:
+    print("It is odd!")
