eef560dd1e53e90a9003c7616d1c3fb1ad79aae0
NurseQ <jamesleroyq@gmail.com>
2018-02-20 00:55:20 +0000

    Script to tell if it is tuesday

---
+++ isItTuesday.py
@@ -0,0 +1,10 @@
+# Script to tell if it is Tuesday
+# Programming and scripting 2018 Ian Mcloughlin
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+    print("Yes it is Tuesday")
+
+else:
+    print("Not yet Tuesday")
