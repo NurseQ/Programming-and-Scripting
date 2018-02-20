fbd5656cb1d4f4815734209cbee611e6b98e1666
NurseQ <jamesleroyq@gmail.com>
2018-02-20 00:50:22 +0000

    if, else practice script

---
+++ ifExercise.py
@@ -0,0 +1,14 @@
+#if statement practice 
+#adapted from https://docs.python.org/3/tutorial/controlflow.html
+
+x = int(input("Please enter your age: "))
+
+if x <= 20:
+    x = 0
+    print("You are young!")
+elif x <= 30:
+    print("You are getting old!")
+elif x <= 40:
+    print("You are getting old!")
+else:
+    print("You are old!")
