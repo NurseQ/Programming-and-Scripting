e16f709edb96b980df1d2e8968e648cf63b2a79d
James Q <jamesleroyq@gmail.com>
2018-03-03 20:29:07 +0000

    removes new line in selected column

--- splitting.py
+++ splitting.py
@@ -3,6 +3,6 @@
 
 with open("datum/iris.csv") as f:
     for line in f: 
-        print(line.split(',')[0]) # prints to the terminal the first column of data
+        print(line.split(',')[4],end='') # removes new line
 
 
