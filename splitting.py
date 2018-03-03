2dbd085b0ad680f608b4265d95871603e4ab2665
James Q <jamesleroyq@gmail.com>
2018-03-03 20:05:46 +0000

    Changes in print to remove new line

--- splitting.py
+++ splitting.py
@@ -3,11 +3,6 @@
 
 with open("datum/iris.csv") as f:
     for line in f: 
-        print(line) #adds another new line 
+        print(line, end='') # removes the new line in code
 
 
-# adding print(line, end='') removes the new line in code above
-
-# adding print(line.split(','), end='') places each line into a python list
-
-# adding print(line.split(',')[0]) prints to the terminal the first column of data
