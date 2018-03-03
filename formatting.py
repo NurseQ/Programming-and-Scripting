f098f1911e5d0a227112f29da9c0dd7ef693b6b4
NurseQ <jamesleroyq@gmail.com>
2018-03-03 19:49:05 +0000

    Delete formatting.py

--- formatting.py
+++
@@ -1,5 +0,0 @@
-#James Quintin 02/03/18
-#This code prints out the outcome of the loop right justified
-for i in range(1,11):
-    print("{:2d} {:3d} {:4d} {:5d}" .format(i, i**2, i**3, i**4))
-    
