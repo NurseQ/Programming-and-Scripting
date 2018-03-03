93d965ba4d486064702cb78ca2c8dfafb12f683a
James Q <jamesleroyq@gmail.com>
2018-03-03 20:00:07 +0000

    Using split() to separate a string into components

--- splitting.py
+++ splitting.py
@@ -3,6 +3,11 @@
 
 with open("datum/iris.csv") as f:
     for line in f: 
-        print(line, end='') # removes the new line in code 
+        print(line) # adds another new line 
 
 
+# adding print(line, end='') removes the new line in code above
+
+# adding print(line.split(','), end='') places each line into a python list
+
+# adding print(line.split(',')[0]) prints to the terminal the first column of data
