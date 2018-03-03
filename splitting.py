bf239434c8952b7d2932d26043b83a3257ce3116
NurseQ <jamesleroyq@gmail.com>
2018-03-03 19:57:55 +0000

    Delete splitting.py

--- splitting.py
+++
@@ -1,8 +0,0 @@
-# James Quintin 03/02/18
-# Using split() to separate string component
-
-with open("datum/iris.csv") as f:
-    for line in f: 
-        print(line, end='') # removes the new line in code 
-
-
