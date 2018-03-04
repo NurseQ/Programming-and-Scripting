10f545c4f06c926e711c41f06642d730d7dfd88e
James Q <jamesleroyq@gmail.com>
2018-03-04 01:51:39 +0000

    removed comma

--- exercise5.py
+++ exercise5.py
@@ -7,5 +7,5 @@ with open("datum/iris.csv") as f:
         
         line = line.split(',')
         
-        print('{:^12}{:^16}{:^8}{:^15}{:15}'.format(line[0],line[1],line[2],line[3],line[4]),  )
+        print('{:^12}{:^16}{:^8}{:^15}{:15}'.format(line[0],line[1],line[2],line[3],line[4])  )
         
