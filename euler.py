865a7f38db680378d2f668f0b585cf1f145f34be
NurseQ <jamesleroyq@gmail.com>
2018-04-02 16:32:46 +0100

    changed code to an adapted version from the internet.
    Seems easiest code to follow, although 9 years old.

--- euler.py
+++ euler.py
@@ -1,8 +1,19 @@
+#James Quintin 02/04/18
+#python code for project euler problem no. 5
+#adapted from https://gist.github.com/PEZ/47534 
 
-sum = 0
-for i in range(2,21):    
-    sum = i * i 
-     
-    print(sum)
-    
+
+def gcd(a, b):
+  while b > 0:
+    a, b = b, a % b
+  return a
+
+def lcm(a, b):
+  return (a * b) / gcd(a, b)
+
+llcm = lcm(11, 12)
+for n in range(12, 20):
+  llcm = lcm(n, llcm)
+
+print (llcm)
     
