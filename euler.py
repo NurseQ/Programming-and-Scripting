38095a433cc89128570d4de3df1f79e1ba60bbf9
James Q <jamesleroyq@gmail.com>
2018-04-03 00:34:21 +0100

    Addes comments to explain script further

--- euler.py
+++ euler.py
@@ -2,15 +2,20 @@
 #python code for project euler problem no. 5
 #adapted from https://gist.github.com/PEZ/47534 
 
+# This script first defines a function to return gcd, then calculate lcm
+# function to calculate greatest common divisor
 
 def gcd(a, b):
+  #this while loop uses less lines rather than with if statement
   while b > 0:
     a, b = b, a % b
   return a
 
+#this function calculates the lowest common multiple of two ints
 def lcm(a, b):
   return (a * b) / gcd(a, b)
 
+#calculates lcm of range of numbers
 llcm = lcm(11, 12)
 for n in range(12, 20):
   llcm = lcm(n, llcm)
