cacd4fe3a0d47c64c8ecfed2a8aebe6348a0807e
James Q <jamesleroyq@gmail.com>
2018-04-03 10:44:36 +0100

    Added comments, added .strip to fix formatting issues

--- exercise5.py
+++ exercise5.py
@@ -1,15 +1,18 @@
 # James Quintin 03/03/18
 # Exercise 5 
 
-# code to open a file, without need for closing the file
+# command to open file without need for closing the file
 with open("datum/iris.csv") as f:
     # print as a header for the values
     print("petal length " "petal width " "sepal length " "sepal width "" Iris Name")
     
-    # loops thru the contents of the file
+    # reads contents of the file line by line, fixes formatting error, rather than using end command.
     for line in f:
-        # splits the content into a list
+        #removes \n from end of each list
+        line = line.strip()
+        # splits each line into separate list
         line = line.split(',')
         
-        print('{:^12}{:^16}{:^8}{:^15}{:15}'.format(line[0],line[1],line[2],line[3],line[4])  )
+        #prints spaces between strings, columns, Trial and error.
+        print('{:^12}{:^14}{:^10}{:^15}{:15}'.format(line[0],line[1],line[2],line[3],line[4],))
         
